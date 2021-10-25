677cf6bcd27ba1b0230a0c11f2083c6ebc4ab74c
weicheng lee <z82111712@gmail.com>
2021-10-24 21:53:29 -0700

    finish intermed

--- fundamentals/fundamentals/fun_inter_I/intermediateI.py
+++ fundamentals/fundamentals/fun_inter_I/intermediateI.py
@@ -1,3 +1,4 @@
+"""
 x = [ [5,2,3], [10,8,9] ]
 students = [
     {'first_name':  'Michael', 'last_name' : 'Jordan'},
@@ -13,3 +14,26 @@ x[1][0] = 15
 students[0]['last_name'] = "Bryant"
 sports_directory['soccer'][0] = "Andres"
 z[0]['y'] = 30
+"""
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterateDictionary(names):
+    for name in names:
+        print(f"first_name - {name['first_name']}, last_name - {name['last_name']}")
+
+def iterateDictionary2(key_name, some_list):
+    for item in some_list:
+        print(item[key_name])
+
+def printInfo(some_dict):
+    for key in some_dict:
+        print(f"{len(key)} {key}")
+        for item in some_dict[key]:
+            print(item)
+        print("")
