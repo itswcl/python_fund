71e2b9c24db93cf57904083036f78a9d9aab5e9b
weicheng lee <z82111712@gmail.com>
2021-10-24 20:42:16 -0700

    update countdown

--- fundamentals/fundamentals/function_basic_ii/function_basic_ii.py
+++ fundamentals/fundamentals/function_basic_ii/function_basic_ii.py
@@ -2,6 +2,11 @@
 Countdown - Create a function that accepts a number as an input. Return a new list that counts down by one, from the number (as the 0th element) down to 0 (as the last element).
 Example: countdown(5) should return [5,4,3,2,1,0]
 """
+def countdown(n):
+    result = []
+    for i in range(n, -1, -1):
+        result.append(i)
+    return result
 """
 Print and Return - Create a function that will receive a list with two numbers. Print the first value and return the second.
 Example: print_and_return([1,2]) should print 1 and return 2
