81c28a2327de7285c1237db44ad24534dda023bb
weicheng lee <z82111712@gmail.com>
2021-10-27 11:43:09 -0700

    initial the pets project

---
+++ fundamentals/oop/dojo_pets/dojo_pets.py
@@ -0,0 +1,16 @@
+class Ninja:
+    pass
+# implement __init__( first_name , last_name , treats , pet_food , pet )
+# implement the following methods:
+# walk() - walks the ninja's pet invoking the pet play() method
+# feed() - feeds the ninja's pet invoking the pet eat() method
+# bathe() - cleans the ninja's pet invoking the pet noise() method
+
+class Pet:
+    pass
+# implement __init__( name , type , tricks ):
+# implement the following methods:
+# sleep() - increases the pets energy by 25
+# eat() - increases the pet's energy by 5 & health by 10
+# play() - increases the pet's health by 5
+# noise() - prints out the pet's sound
