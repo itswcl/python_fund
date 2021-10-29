592beb844df2174bcce0d6facf089fc03e7dd361
weicheng lee <z82111712@gmail.com>
2021-10-28 17:08:31 -0700

    finish checkerboard with x/y

--- flask/fundamentals/checkerboard/server.py
+++ flask/fundamentals/checkerboard/server.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/<int:x>")
+def num_chart_x(x):
+    return render_template("index.html", x=x)
+
+@app.route("/<int:x>/<int:y>")
+def num_chart_y(x, y):
+    return render_template("index.html", x=x, y=y)
+
+@app.route("/<int:x>/<int:y>/<string:color1>")
+def num_chart_color(x, y, color1):
+    return render_template("index.html", x=x, y=y, color1=color1)
+
+@app.route("/<int:x>/<int:y>/<string:color1>/<string:color2>")
+def num_chart_double_color(x, y, color1, color2):
+    return render_template("index.html", x=x, y=y, color1=color1, color2=color2)
+
 if __name__ == "__main__":
     app.run(debug=True)
