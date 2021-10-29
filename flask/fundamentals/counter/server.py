c3dd5e2fd7f1b46fcf50ffa80727dae24b84322a
weicheng lee <z82111712@gmail.com>
2021-10-29 15:00:34 -0700

    counting refresh, need to add reset

--- flask/fundamentals/counter/server.py
+++ flask/fundamentals/counter/server.py
@@ -1,9 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+app.secret_key = "this is the WCL"
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+
+@app.route("/", methods=["GET"])
+def form_submit():
+
+    if session["click"]:
+        session["click"] += 1
+    else:
+        session["click"] = 1
+
+    return render_template("index.html", number=session["click"])
+
+# @app.route("/")
+# def clear():
+#     session.clear()
+#     return render_template(
+#         "index.html",
+#         number = session["number"]
+#     )
 
 if __name__ == "__main__":
     app.run(debug=True)
