2d907d9c8209d28f8a4bcd2248cdc304a1cb273d
weicheng lee <z82111712@gmail.com>
2021-10-30 14:20:03 -0700

    update main index with request info pass on to session

--- flask/fundamentals/dojo_survey/server.py
+++ flask/fundamentals/dojo_survey/server.py
@@ -1,14 +1,35 @@
 from flask import Flask, render_template, redirect, request, session
 
 app = Flask(__name__)
+app.secret_key = "this is new test"
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/submit", methods=["POST"])
+def submit():
+
+    session["name"] = request.form["name"]           # assign request value to session
+    session["location"] = request.form["location"]
+    session["language"] = request.form["language"]
+    session["comment"] = request.form["comment"]
+
+    return redirect ("/result")
+
 @app.route("/result")
 def result():
-    pass
+    name = session["name"]
+    location = session["location"]
+    language = session["language"]
+    comment = session["comment"]
+
+    return render_template("result.html",
+        name = name,
+        location = location,
+        language = language,
+        comment = comment
+        )
 
 if __name__ == "__main__":
     app.run(debug=True)
