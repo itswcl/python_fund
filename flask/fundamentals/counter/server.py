325fb54d201f6959b5b3e408cb136f731c79dc65
weicheng lee <z82111712@gmail.com>
2021-10-29 20:54:01 -0700

    add 2 view button and reset

--- flask/fundamentals/counter/server.py
+++ flask/fundamentals/counter/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, session
+
 app = Flask(__name__)
 app.secret_key = "this is the WCL"
 
@@ -6,9 +7,8 @@ app.secret_key = "this is the WCL"
 # def index():
 #     return render_template("index.html")
 
-@app.route("/", methods=["GET"])
+@app.route("/")
 def form_submit():
-
     if session["click"]:
         session["click"] += 1
     else:
@@ -16,13 +16,22 @@ def form_submit():
 
     return render_template("index.html", number=session["click"])
 
-# @app.route("/")
-# def clear():
-#     session.clear()
-#     return render_template(
-#         "index.html",
-#         number = session["number"]
-#     )
+@app.route("/clear")
+def clear_out():
+    session["click"] = 0
+    render_template("index.html", number=session["click"])
+    return redirect("/")
+
+@app.route("/", methods=["POST"])
+def form_click():
+    if request.form["click"] == "click":
+        session["click"] += 1
+    else:
+        request.form["click"] == "reset"
+        session["click"] = 0
+
+    render_template("index.html", number=session["click"])
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
