dde59a20d03e921159c9f13ae50a020f498785b0
weicheng lee <z82111712@gmail.com>
2021-11-03 13:13:57 -0700

    update user html

--- flask_mysql/crud/users_cr/server.py
+++ flask_mysql/crud/users_cr/server.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, redirect, request
+from user import User
 
 app = Flask(__name__)
 app.secret_key = "TEST"
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    all_users = User.get_all()
+
+    return render_template('index.html', all_users=all_users)
+
 if __name__ == '__main__':
     app.run(debug=True)
