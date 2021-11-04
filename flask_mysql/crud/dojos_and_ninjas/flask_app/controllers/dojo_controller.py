c3d5641561ee227439fd62026bb708f2b2c2d005
weicheng lee <z82111712@gmail.com>
2021-11-04 16:15:18 -0700

    display dojo names

--- flask_mysql/crud/dojos_and_ninjas/flask_app/controllers/dojo_controller.py
+++ flask_mysql/crud/dojos_and_ninjas/flask_app/controllers/dojo_controller.py
@@ -1,3 +1,8 @@
 from flask_app import app
 from flask import render_template, redirect, request, session
 from flask_app.models.dojo import Dojo
+
+@app.route("/")
+def index():
+    # Dojo.display_all()
+    return render_template("index.html", dojos = Dojo.display_all())
--- flask_mysql/crud/dojos_and_ninjas/flask_app/models/dojo.py
+++ flask_mysql/crud/dojos_and_ninjas/flask_app/models/dojo.py
@@ -1,1 +1,22 @@
 from flask_app.config.mysqlconnection import connectToMySQL
+
+class Dojo:
+    def __init__(self, data):
+        self.name = data["name"]             #assign self variable for all the return value
+        self.created_at = data["created_at"]
+        self.updated_at = data["updated_at"]
+
+    # CRUD ControllerReadUpdateDelete
+
+    # READ all db of Dojos
+    @classmethod
+    def display_all(cls):
+        query = "SELECT * FROM dojos;" # query declartion
+
+        results = connectToMySQL("dojos_and_ninjas_schema").query_db(query) # getting back the row of results
+        dojos = []
+        print(results)
+        for row in results:
+            dojos.append(Dojo(row))
+        return dojos
+
