6afff9c3b573434334df28dd73df85fafb54eddb
weicheng lee <z82111712@gmail.com>
2021-11-06 13:38:57 -0700

    fixed query wording and have add email request to SQL

--- flask_mysql/validation/email_validation/flask_app/controllers/email_controller.py
+++ flask_mysql/validation/email_validation/flask_app/controllers/email_controller.py
@@ -1,4 +1,3 @@
-from flask.templating import render_template_string
 from flask_app import app # import our initialing app
 from flask import render_template, redirect, request # import render/redirect/request for route purpose
 
@@ -7,3 +6,12 @@ from flask_app.models.email_model import Email # import the Class
 @app.route("/") # initial the index page
 def index():
     return render_template("index.html")
+
+@app.route("/email/form") # route for form
+def form():
+    return render_template("index.html")
+
+@app.route("/email/add", methods=["POST"]) # route for POST request
+def add():
+    Email.add_new_email(request.form)
+    return redirect("/")
--- flask_mysql/validation/email_validation/flask_app/models/email_model.py
+++ flask_mysql/validation/email_validation/flask_app/models/email_model.py
@@ -7,9 +7,10 @@ class Email:
         self.created_at = data["created_at"]    # from ERD order
         self.updated_at = data["updated_at"]    # from ERD order
 
+    # UPDATE
     @classmethod
     def add_new_email(cls, data):
-        query = "INSERE INTO emails (email, created_at, update_at) VALUE (%(email)s, NOW(), NOW())"
+        query = "INSERT INTO emails (email, created_at, updated_at) VALUE (%(email)s, NOW(), NOW())"
 
         results = connectToMySQL("email_schema").query_db(query ,data)
 
